e373735b8f801f2370eb1230f93d7de91ddbf1f4
DeliriousEC <m-trankov@mail.ru>
2023-12-19 01:55:50 +0300

    Проблема с кнопкой

---
+++ src/components/button/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  color: red;
+  backgroud-color: red;
+`;
+
+export const Button = () => {
+  return <StyledButton>Click me!</StyledButton>;
+};
